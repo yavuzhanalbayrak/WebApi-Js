66c3cc7b95ce29dc831471db12beda1fe84e9587
Yavuzhan ALBAYRAK <yavuzalbayrak55@gmail.com>
2025-02-02 13:27:41 +0300

    fix: Port fixed

--- src/app.js
+++ src/app.js
@@ -26,8 +26,7 @@ app.use((req, res, next) => {
 
 // app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(() => {
   // info(`🚀 Server running on http://localhost:${PORT}`);
   console.log(`🚀 Server running on http://localhost:${env.PORT}`);
 });
