72c07d72e761634a0fe971b3f1f6975283537d64
Yavuzhan ALBAYRAK <yavuzalbayrak55@gmail.com>
2025-02-03 23:14:25 +0300

    feat: i18n added

--- src/controllers/healthCheckController.js
+++ src/controllers/healthCheckController.js
@@ -3,8 +3,11 @@ import Result from "../utils/result.js";
 
 export const getHealthCheck = async (req, res, next) => {
   try {
-    const response = await healthCheck(req.query);
-    res.status(200).json(Result.successResponse(response));
+    // const response = await healthCheck(req.query);
+    // res.status(200).json(Result.successResponse(response));
+
+    res.status(200).json(Result.successResponse(`${req.t("test.testSuccess")}`));
+
   } catch (error) {
     next(error);
   }
