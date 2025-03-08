5ab3524e42b39e50464e4b942426f0b3f9c165d1
Yavuzhan ALBAYRAK <yavuzalbayrak55@gmail.com>
2025-03-08 21:33:40 +0300

    fix: Correct environment variable loading in env.js to support multiple environments

--- src/config/env.js
+++ src/config/env.js
@@ -1,10 +1,23 @@
 import dotenv from "dotenv";
+import path from "path";
+import logger, { logRequests } from "../config/logger.js";
 
 dotenv.config();
 
-const env = {
+const envFile = `.env.${process.env.NODE_ENV || "development"}`;
+dotenv.config({ path: path.resolve(process.cwd(), envFile) });
+
+const requiredEnvVars = ["MONGO_URI", "PORT"];
+requiredEnvVars.forEach((key) => {
+  if (!process.env[key]) {
+    logger.warn(`⚠️ Uyarı: ${key} değişkeni tanımlı değil.`);
+  }
+});
+
+const config = {
   PORT: process.env.PORT || 5000,
-  MONGO_URI: process.env.MONGO_URI,
+  MONGO_URI: process.env.MONGO_URI || "",
+  NODE_ENV: process.env.NODE_ENV || "development",
 };
 
-export default env;
+export default config;
